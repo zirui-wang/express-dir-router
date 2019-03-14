b48bea318cc826f6eee882b2e4eadbd6e2ae1769
zirui-wang <zirui.zirui.wang.wang@gmail.com>
2019-03-14 12:44:57 -0700

    feat: implement dir modular route.

--- README.md
+++ README.md
@@ -6,6 +6,19 @@
 $ npm install express-dir-router
 ```
 
+## Usage
+
+```js
+const app = require('express')();
+
+const router = require('express-dir-router')({
+  app: app,
+  root: __dirname,
+  prefix: '/api'
+});
+app.use(router);
+```
+
 ## License
 
 MIT
--- index.js
+++ index.js
@@ -1,5 +1,74 @@
 'use strict';
 
-module.exports = function() {
-  console.log('Init.');
+const router = require('express').Router();
+const path = require('path');
+const requireDir = require('require-dir');
+
+function flattenObject(obj) {
+  const toReturn = {};
+
+  for (let i in obj) {
+    if (!obj.hasOwnProperty(i)) continue;
+
+    if (typeof obj[i] == 'object') {
+      const flatObject = flattenObject(obj[i]);
+      for (let x in flatObject) {
+        if (!flatObject.hasOwnProperty(x)) continue;
+
+        toReturn[i + '/' + x] = flatObject[x];
+      }
+    } else {
+      toReturn[i] = obj[i];
+    }
+  }
+  return toReturn;
+}
+
+function getBaseUrl(prefix, name) {
+  const index = name.lastIndexOf('/');
+  if (index === -1) return prefix;
+  return prefix + '/' + name.substring(0, index);
+}
+
+function dispatch(baseUrl, route) {
+  const { method, url, handler } = route;
+  const curUrl = baseUrl + url;
+
+  switch (method.toLowerCase()) {
+    case 'get':
+      router.get(curUrl, handler);
+      break;
+    case 'post':
+      router.post(curUrl, handler);
+      break;
+    case 'put':
+      router.put(curUrl, handler);
+      break;
+    case 'delete':
+      router.delete(curUrl, handler);
+      break;
+    case 'patch':
+      router.patch(curUrl, handler);
+    default:
+      break;
+  }
+}
+
+module.exports = config => {
+  const { app = {}, root = '', prefix = '' } = config;
+
+  const dir = flattenObject(
+    requireDir(path.resolve(root, 'routes'), {
+      recurse: true,
+      filter: function(fullPath) {
+        return !fullPath.endsWith('index.js');
+      }
+    })
+  );
+
+  for (let name in dir) {
+    dispatch(getBaseUrl(prefix, name), dir[name](app));
+  }
+
+  return router;
 };
